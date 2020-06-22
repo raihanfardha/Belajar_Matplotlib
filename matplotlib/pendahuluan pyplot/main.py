4f222c11cc2ec01bf843af1b9cb9a982a39fc78a
raihanfardha <57145922+raihanfardha@users.noreply.github.com>
2020-06-22 10:11:20 +0700

    Add files via upload

---
+++ matplotlib/pendahuluan pyplot/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#buat data
+x = np.array([1,2,3,4,5])
+y = np.array([1,4,9,16,25])
+y2 = np.array([1,16,81,256,625])
+
+#buat plot
+plt.plot(x,y)
+plt.plot(x,y2)
+
+#tampilkan plot
+plt.show()
